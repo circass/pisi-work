5d8afaa24fa0b8693e37f77e41844380cb6017fd
circass <erdincgultekin@gmail.com>
2013-01-16 17:05:55 +0200

    new

---
+++ kaptan/src/kaptan/screens/scrGoodbye.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005-2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+from PyKDE4.kdecore import ki18n, KGlobal, KConfig
+
+import subprocess, sys
+
+from kaptan.screen import Screen
+from kaptan.screens.ui_scrGoodbye import Ui_goodbyeWidget
+
+class Widget(QtGui.QWidget, Screen):
+    title = ki18n("More")
+    desc = ki18n("Congratulations!")
+
+    def __init__(self, *args):
+        QtGui.QWidget.__init__(self,None)
+        self.ui = Ui_goodbyeWidget()
+        self.ui.setupUi(self)
+
+        lang = KGlobal.locale().language()
+        if lang == "tr":
+            self.helpPageUrl = "http://www.pardus-anka.org"
+        else:
+            self.helpPageUrl = "http://worldforum.pardus-linux.nl"
+
+    def on_buttonSystemSettings_clicked(self):
+        self.procSettings = QProcess()
+        self.procSettings.start("systemsettings")
+
+    def on_buttonHelpPages_clicked(self):
+        self.procSettings = QProcess()
+        command = "kfmclient openURL " + self.helpPageUrl
+        self.procSettings.start(command)
+
+    def execute(self):
+       return True
+
+
